c660cff467d683a3e2c2945cf24f3bbaa0fdf380
Erum Afzal <40553436+ErumAfzal@users.noreply.github.com>
2026-07-14 11:17:29 +0200

    Define enums for communication and social concepts
    Added enumerations for communication types, social roles, conversation intentions, languages, and experimental conditions.

--- constants.py
+++ constants.py
@@ -1,1 +1,29 @@
+from enum import Enum
 
+
+class CommunicationType(str, Enum):
+    STRATEGIC = "strategic"
+    UNDERSTANDING_ORIENTED = "understanding_oriented"
+
+
+class SocialRole(str, Enum):
+    STRONGER = "stronger"
+    EQUAL = "equal"
+    WEAKER = "weaker"
+
+
+class ConversationIntention(str, Enum):
+    CONTENT_GOAL = "content_goal"
+    RELATIONSHIP_GOAL = "relationship_goal"
+
+
+class Language(str, Enum):
+    GERMAN = "de"
+    ENGLISH = "en"
+
+
+class ExperimentalCondition(str, Enum):
+    C1_PROMPT_ONLY = "C1_prompt_only"
+    C2_STRUCTURED_PROMPT = "C2_structured_prompt"
+    C3_ONTOLOGY_GROUNDED = "C3_ontology_grounded"
+    C4_ONTOLOGY_REASONING = "C4_ontology_reasoning"
